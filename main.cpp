e9c00805cbf331133c51ecf3d31f879d35f4ce34
ZiyueWang675 <ziyue_wang@ucsb.edu>
2021-11-06 20:05:43 -0700

    change

--- main.cpp
+++ main.cpp
@@ -11,4 +11,10 @@ int main(int argc, char *argv[])
     string type_implement = argv[1];
     string f_insert = argv[2];
     string remove = argv[3];
+    if (type_implement == "heap")
+    {
+    }
+    else if (type_implement == "minmedianmax")
+    {
+    }
 }
