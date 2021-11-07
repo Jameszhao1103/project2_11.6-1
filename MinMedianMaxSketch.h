1fc1b93a30c712dbc9506693413a9bc089e8a925
ZiyueWang675 <ziyue_wang@ucsb.edu>
2021-11-07 00:15:46 -0700

    second

--- MinMedianMaxSketch.h
+++ MinMedianMaxSketch.h
@@ -1,27 +1,33 @@
-#ifdef MINMEDIANMAXSKETCH_H
+#ifndef MINMEDIANMAXSKETCH_H
 #define MINMEDIANMAXSKETCH_H
 
+#include <vector>
 template <class T>
 class minMedianMax
 {
 private:
-    int minSize;
-    int maxSize;
+    int minSize = 0;
+    int maxSize = 0;
     T min;
     T max;
     std::vector<T> min_heap;
     std::vector<T> max_heap;
 
 public:
-    minMedianMax(T max[], T min[]);
-    void insert(T el);
+    minMedianMax(int inMinSize, int inMaxSize, T min[], T max[]);
+    void insert(bool isMin, T el);
+    void BubbleDown(bool (*func)(T a, T b), int index, std::vector<T> heap);
+    void BubbleUp(bool (*func)(T a, T b), int index, std::vector<T> heap);
     void remove(T el);
+    void heapify(bool isMin, int size);
     T get_median();
     T get_minimum();
     T get_maximum();
     int get_size();
     bool search(T el);
-    bool compare(T a, T b);
+    bool minComparer(T a, T b);
+    bool maxComparer(T a, T b);
+    bool isBalanced();
 };
 
 #endif
